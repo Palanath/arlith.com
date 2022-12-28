e589f2755c435f9f37a371cd749a994cc5067b17
Palanath <pala@dusttoash.org>
2022-12-27 18:31:11 -0600

    Add homepage to Arlith wiki

---
+++ arlith.net/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="index.css">
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+<h1>Arlith Wiki</h1>
+</body>
+</html>
