d92a4e7de2efabf0227076f2f340864b7335fab2
Palanath <pala@dusttoash.org>
2022-12-27 23:00:27 -0600

    Add a simple paragraph to the wiki homepage

--- arlith.net/index.php
+++ arlith.net/index.php
@@ -6,6 +6,14 @@
 <title></title>
 </head>
 <body>
-<h1>Arlith Wiki</h1>
+	<h1>Arlith Wiki</h1>
+	<p>
+		The <b>Arlith Wiki</b> is an online informational encyclopedia of
+		documentation on Arlith's codebase. The purpose of the encyclopedia is
+		to act as a central repository of documentation for the design of the
+		software components and software systems that comprise Arlith's
+		codebase. Although not truly a wiki, the encyclopedia is referred to
+		as "the wiki" for short in context.
+	</p>
 </body>
 </html>
