15282f00a03c07f0fc799c41f7873b02425e8f2e
Palanath <pala@dusttoash.org>
2022-12-28 00:03:10 -0600

    Update wiki homepage

--- arlith.net/index.php
+++ arlith.net/index.php
@@ -8,12 +8,31 @@
 <body>
 	<h1>Arlith Wiki</h1>
 	<p>
-		The <b>Arlith Wiki</b> is an online informational encyclopedia of
+		The <b>Arlith Wiki</b> is an online, informational encyclopedia of
 		documentation on Arlith's codebase. The purpose of the encyclopedia is
-		to act as a central repository of documentation for the design of the
-		software components and software systems that comprise Arlith's
+		to act as a central repository of documentation for the design of the (1)
+		software components and (2) software systems, that comprise Arlith's
 		codebase. Although not truly a wiki, the encyclopedia is referred to
-		as "the wiki" for short in context.
+		as a wiki, or "the wiki," owing to its official name.
+	</p>
+	<p>
+		The wiki is contained within a <a
+			href="https://github.com/Palanath/arlith.com">GitHub repository</a>
+		alongside all of Arlith's other sibling sites, (e.g. <code>arlith.com</code>).
+		The website is hosted separately (i.e., not on GitHub).
+	</p>
+	<h2>Navigation</h2>
+	<p>
+		The primary means of navigating the wiki is through the <a
+			href="/index">index page</a>, which comprises a full list of all the
+		pages contained by the wiki.
+	</p>
+	<h2>Editing</h2>
+	<p>
+		Editing or adding a page to the wiki consists of editing or adding the
+		page's <i>index resource</i>, which contains its source code. A formal
+		guide on wiki editing can be found on the <a href="/editing">editing
+			page</a>.
 	</p>
 </body>
 </html>
