9d9d174917c97db3bcc4a7d1e90754a229dbf89e
Palanath <pala@dusttoash.org>
2022-12-28 00:31:47 -0600

    Add wiki-editing page (using wiki template) to wiki

---
+++ arlith.net/editing/index.php
@@ -0,0 +1,9 @@
+<?php h("Arlith Wiki - Editing a page");?><h1>Wiki Editing</h1>
+<p>
+	Editing the wiki consists of adding new pages or editing existing
+	pages. Each page in the wiki is comprised of a sub-directory of the <code>arlith.net</code>
+	folder contained within the wiki's source code repository. The page's
+	source code is contained inside an <code>index</code> resource in that
+	sub-directory.
+</p>
+<?php t();?>
