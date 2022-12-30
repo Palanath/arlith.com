daef034e3865aa557aad9dddfababc84b7e6ff2f
Palanath <pala@dusttoash.org>
2022-12-29 21:39:30 -0600

    Add wiki styling information on headers

--- arlith.net/editing/index.php
+++ arlith.net/editing/index.php
@@ -26,5 +26,42 @@
 	wiki-themed CSS stylesheet. Not all HTML elements are handled by the
 	stylesheet. This section shows what elements are styled by the wiki's
 	stylesheet and how they appear.</p>
-
+<h3>Headers</h3>
+<p>
+	Headers come in 6 different <i>levels</i>, which affect their size and
+	their semantic importance on a page.
+</p>
+<table>
+	<tr>
+		<th>HTML</th>
+		<th>Rendered Content</th>
+	</tr>
+	<tr>
+		<td><pre><code>&lt;h1&gt;Heading 1&lt;/h1&gt;
+&lt;h2&gt;Heading 2&lt;/h2&gt;
+&lt;h3&gt;Heading 3&lt;/h3&gt;
+&lt;h4&gt;Heading 4&lt;/h4&gt;
+&lt;h5&gt;Heading 5&lt;/h5&gt;
+&lt;h6&gt;Heading 6&lt;/h6&gt;</code></pre></td>
+		<td><h1>Heading 1</h1>
+			<h2>Heading 2</h2>
+			<h3>Heading 3</h3>
+			<h4>Heading 4</h4>
+			<h5>Heading 5</h5>
+			<h6>Heading 6</h6></td>
+	</tr>
+</table>
+<h4>Presentational Notes</h4>
+<ul>
+	<li>Headings <code>&lt;h1&gt;</code> and <code>&lt;h2&gt;</code> have a
+		serif font and have a separator line immediately under them.
+	</li>
+	<li>All other (lesser) headings are sans-serif, smaller, and have no
+		line.</li>
+</ul>
+<h4>Semantic Notes</h4>
+<ul>
+	<li>Conventional wiki semantics dictate that the top of every page have
+		a level 1 heading which defines the page's title.</li>
+</ul>
 <?php t();
