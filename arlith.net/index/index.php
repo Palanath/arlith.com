4d4cbd61271e587f1910f9d431844053803a012d
Palanath <pala@dusttoash.org>
2022-12-29 23:51:17 -0600

    Add the index page to the wiki

---
+++ arlith.net/index/index.php
@@ -0,0 +1,12 @@
+<?php h("Arlith Wiki - Wiki index page/table of contents");?>
+<h1>Wiki Index</h1>
+<p>This page exhaustively indexes every page on the wiki. It serves as a
+	reference for where each other page is located.</p>
+<ul>
+	<li><a href="/">Home</a></li>
+	<li><a href="/index">Index</a> - Table of contents/exhaustive list of
+		wiki pages.</li>
+	<li><a href="/editing">Wiki Editing</a> - Explanation of how to edit
+		the wiki.</li>
+</ul>
+<?php t();
