bf7c29467638593dd904ed236596dc4e8ca1d4b3
Palanath <pala@dusttoash.org>
2023-01-13 00:39:16 -0600

    [Wiki] Update change-email request page

--- arlith.net/request-index/change-email/index.php
+++ arlith.net/request-index/change-email/index.php
@@ -4,3 +4,23 @@
 	their account. After successfully invoking the request, the email
 	address associated with the invoker's account will be the address
 	specified in the request.</p>
+<div class="blurb info">
+	This request requires the connection to be <a
+		href="/authorized-connection">authorized</a>.
+</div>
+<h2>Structure</h2>
+<p>This request has the following parameters:</p>
+<table class="request-parameters">
+	<tr>
+		<th>Parameter Name</th>
+		<th>Type</th>
+		<th>Description</th>
+		<th>Optional?</th>
+	</tr>
+	<tr>
+		<td>Email</td>
+		<td>TextValue</td>
+		<td>The new email to associate with this account.</td>
+		<td data-optional=no></td>
+	</tr>
+</table>
