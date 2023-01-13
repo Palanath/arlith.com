ad8892f386fef0ad3cd44ffbbff59754b91135ab
Palanath <pala@dusttoash.org>
2023-01-12 23:55:18 -0600

    [Wiki] Create change-email page in request-index

---
+++ arlith.net/request-index/change-email/index.php
@@ -0,0 +1,6 @@
+<?php h("Arlith Wiki - Change email request documentation");?>
+<h1>Change Email Request</h1>
+<p>This request allows users to change the email address associated with
+	their account. After successfully invoking the request, the email
+	address associated with the invoker's account will be the address
+	specified in the request.</p>
