cce84e43ab800b13128d8c91ba0e81179add272c
Palanath <pala@dusttoash.org>
2023-01-22 04:15:09 -0600

    Update email-syntax page

--- arlith.net/email-syntax/index.php
+++ arlith.net/email-syntax/index.php
@@ -28,25 +28,27 @@
 </style>
 <?php h2();?>
 <h1>Email Syntax</h1>
-<p>This page documents what Arlith considers to be a valid email
-	address. Valid email addresses can be provided through the client when
-	registering for or logging into an account.</p>
-<h2>Production Rules</h2>
 <p>
-	Following are production rules that describe how Arlith parses and
-	validates email addresses. The syntax used in the below code block is
-	similar to that used by RFC, but with color coding and some variations.
-	The syntax of what Arlith considers an actual email address (the <em>meaning</em>
-	of the below code block) is also similar to that defined by RFC, but
-	with some variations (e.g., obsolete syntax is not considered by
-	Arlith). For details on how to interpret the below code block, see
-	underneath it.
+	Arlith parses email addresses similar to their official syntax. This
+	page describes how Arlith validates emails. Emails are typically
+	validated by different parts of the application using the <code>pala.apps.arlith.libraries.Utilities#checkEmailValidity(String)</code>
+	function. Please note that this page does not contain information about
+	<em>interpretation</em> of email addresses; rather it only specifies
+	syntax validation for them.
+</p>
+<p>Arlith's email syntax is designed to encompass nearly all
+	considerably "normal" email addresses while still allowing many unusual
+	formats.</p>
+<h2>Specification</h2>
+<p>
+	Arlith considers an email to be a local part, followed by an <code>@</code>
+	symbol, followed by a domain.
 </p>
 <pre><code><span class="production">email-address</span>   =   <span
 		class="production">local-part</span> <span class="chars">'@'</span> <span
 		class="production">domain</span>
 <span class="production">local-part</span>      =   <span
-		class="production">dot-atom</span> / <span
+		class="described-production">normal-local-part</span> / <span
 		class="described-production">quoted-string</span>
 <span class="production">domain</span>          =   <span
 		class="described-production">domain-name</span> / <span class="chars">'['</span><span
@@ -54,13 +56,17 @@
 <span class="production">ip-addr</span>         =   <span
 		class="described-production">ip-v4</span> / <span
 		class="described-production"
-		style="color: red; text-decoration: line-through;">ip-v6</span>
-<span class="production">dot-atom</span>        =   <span
-		class="production">atext</span><sup class="repl">+</sup> (<span
-		class="chars">'.'</span> <span class="production">atext</span><sup
-		class="repl">+</sup>)<sup class="repl">*</sup>
-<span class="production">atext</span>           =   <span class="chars">'a'-'z'</span> / <span
-		class="chars">'A'-'Z'</span> / <span class="chars">'0'-'9'</span>
+		style="color: red; text-decoration: line-through;">ip-v6</span></code></pre>
+<h3>Local Part</h3>
+<p>
+	A local part can either be <i>quoted</i> or <i>non-quoted</i>.
+	Non-quoted local parts can contain any <i>local-part character</i> and
+	can be split into parts, delimited by periods (<code>.</code>
+	characters):
+</p>
+<pre><code><span class="production">local-part-char</span> =   <span
+		class="chars">'a'-'z'</span> / <span class="chars">'A'-'Z'</span> / <span
+		class="chars">'0'-'9'</span>
                     <span class="chars">'!'</span> / <span class="chars">'#'</span> / <span
 		class="chars">'$'</span> /
                     <span class="chars">'%'</span> / <span class="chars">'&amp;'</span> / <span
@@ -74,71 +80,128 @@
                     <span class="chars">'{'</span> / <span class="chars">'|'</span> / <span
 		class="chars">'}'</span> /
                     <span class="chars">'~'</span></code></pre>
+<p>For example, the following email addresses are all valid:</p>
+<ul>
+	<li><code>pala@arlith.com</code> - Normal email address, simple local
+		part (<code>pala</code>) followed by straightforward domain (<code>arlith.com</code>).</li>
+	<li><code>pala.nath@arlith.com</code> - Normal email with period in
+		local part. The period cannot be at the beginning or end of the local
+		part, nor can two periods touch each other in the local part.</li>
+	<li><code>"stuff in quotes"@arlith.com</code> - Email with quoted local
+		part. With quotations, any character can be included in the local
+		part. However, for double quotations to be contained in the local part
+		without being treated as the "end" of the local part, they need to be
+		prepended with a backslash. Additionally, for a backslash to be
+		contained in the local part without being considered an "escape"
+		character, it should be prepended with another backslash.</li>
+	<li><code>""@arlith.com</code> - Quoted local part with no content.
+		Arlith will accept this email as valid. It considers any string inside
+		the double quotes, that is properly escaped, to be a valid local-part.</li>
+	<li><code>"weird characters in quotes: ((;:'\"\\"@arlith.com</code> -
+		Quoted local part with otherwise-illegal characters. Some of these
+		characters, including <code>(</code>, would not be valid in the local
+		part without double quotations. Since the local part is quoted in this
+		example, the characters can be used in the local part. Note that
+		Arlith does <em>not</em> consider comments in the local part or domain
+		of an email address.</li>
+	<li><code>legal-localpart-chars!#$%^&@arlith.com</code> - Normal local
+		part with unusual (but legal) characters. According to the production
+		rule above (for <code class="production">local-part-char</code>), the
+		characters used in this example's local part do not need to be quoted
+		to be valid.</li>
+	<li><code>!.#.$.%.^.&@arlith.com</code> - Unusual (but legal) chars
+		separated by periods. Apart from the periods, each of these characters
+		is, again, contained in <code class="production">local-part-char</code>
+		above, so is valid. The periods are allowed to "split" the string <code>!#$%^&</code>
+		as a delimiter, which is demonstrated in this example.</li>
+</ul>
+<p>Examples of email addresses with malformed local parts include:</p>
+<ul>
+	<li><code>@arlith.com</code> - No local part.</li>
+	<li><code>.pala@arlith.com</code> - The local part can't begin with a
+		period.</li>
+	<li><code>pala.@arlith.com</code> - The local part can't end with a
+		period, either.</li>
+	<li><code>pala..nath@arlith.com</code> - The local part can't have two
+		adjacent periods.</li>
+	<li><code>"a"b"c"@arlith.com</code> - The local part of this email
+		address is considered to be the characters <code>"a"</code>, as these
+		make up a whole, quoted string. The characters <code>b"c"</code> after
+		those, all the way up to the <code>@</code> symbol, are considered to
+		"separate" the local part from the <code>@</code> symbol, and are not
+		allowed.</li>
+	<li><code>"abc\"@arlith.com</code> - The entire string in this case is
+		considered to be the local part. The backslash before the second
+		quotation mark "escapes" that quotation mark, causing it to simply be
+		a part of the local part. The local part remains unterminated because
+		there's no non-escaped double quotation character to match with the
+		very first character in the email address. If another quotation mark
+		were added after, such as in: <code>"abc\""@arlith.com</code>, the
+		email address would then be valid.</li>
+	<li><code>(abc)@arlith.com</code> - This local part is not quoted but
+		contains characters that are not allowed (the <code>(</code> and <code>)</code>
+		characters).</li>
+</ul>
 <p>
-	Quoted strings are strings beginning and ending with double-quotation
-	marks (<code>"</code>).<sup info=1></sup> Quoted strings may contain
-	any character, but every contained quotation mark, that is not the very
-	first or last character, must be "escaped" by being prepended with one
-	backslash (<code>\</code>). Additionally, only all backslashes whose
-	purposes are not to escape a quotation character or another backslash,
-	must be, themselves, escaped. (This rule is only partially enforced.<sup
-		info=2></sup>)<span info=1>RFC notes that the local-part of an email
-		address should be interpreted by the mail server. This means that
-		unusual email addresses, such as <code>"john.doe"@gmail.com</code>
-		will be syntactically validated by Arlith, but considered literally.
-		In the case of <code>"john.doe"@gmail.com</code>, when sending an
-		email to such an address, the recipient that would be used is
-		literally <code>"john.doe"@gmail.com</code>, with quotes, which might
-		not be what one expects. The corresponding mail server (gmail, in this
-		instance) may reject the request as a result of the email address
-		having quotations. In essence, characters that seem necessary only for
-		syntactical reasons (like backslashes meant for escaping) will not be
-		removed once the email address is parsed. Functionality to allow the
-		user to manipulate this better may be added in the future.
-	</span><span info=2>Arlith requires that literal backslashes in quoted
-		local parts be escaped, but does not reject emails which use
-		backslashes to "escape" characters which need not be escaped. For
-		example, according to the above requirements, the email address <code>"joe\shmoe"@arlith.org</code>
-		is syntactically invalid, because the <code>\</code> character
-		"escapes" the <code>s</code> even though the <code>s</code> does not
-		need escaping. Arlith, however, will accept the string as a valid
-		email address.
+	The <code class="production">domain</code> portion can either be an IP
+	address or an actual domain name.
+</p>
+<h3>Domain Name</h3>
+<p>
+	<code class="described-production">domain-name</code>s are used to
+	represent an actual domain name in Arlith. Currently, Arlith will
+	accept any sequence of two or more <i>domain labels</i> separated by
+	periods (<code>.</code> chars), such that no domain label begins or
+	ends with a hyphen. Domain labels can contain any letter or decimal
+	digit, and can contain hyphens.<sup info=1></sup><span info=1>Normal
+		email addresses are actually allowed to have single-label domain
+		names, but Arlith does not allow this. Single-label domain names are
+		commonly used for referring to servers such as <code>localhost</code>.
 	</span>
 </p>
+<h3>IPv4</h3>
 <p>
-	Colloquially, an email address is the <code>local-part</code> followed
-	by an <code>@</code> symbol and a <code>domain</code> address to locate
-	the mail server. The local-part can be either a quoted string, allowing
-	it to contain essentially any character, or a <code>dot-atom</code>. A
-	dot-atom can be any sequence of <code>atext</code> characters with dot
-	characters (<code>.</code>) <i>in between</i> (not at either endpoint),
-	but <i>not consecutively</i>; (two <code>.</code> characters cannot
-	touch).
+	Arlith supports IPv4 addresses specified using decimal digits in the
+	standard format. Each period-delimited octet is allowed to contain any
+	one decimal number from <code>0</code> to <code>255</code>. Note that
+	Arlith does <b>not</b> allow users to prepend <code>0</code>s in front
+	of an octet; the only case where the leftmost digit of an IPv4 octet
+	may be <code>0</code> is when the octet's value is <code>0</code>
+	itself.<sup info=2></sup><span info=2>The reason for this is because
+		many IP software systems treat octets with a <code>0</code> prefix to
+		be written in octal notation. One notable example is software that
+		delegates parsing IP addresses to the <code>ip_addr</code> function in
+		C, which is used by utilities such as Microsoft Windows's <code>ping</code>
+		script. In such systems, the user may enter <code>021</code> to refer
+		to the decimal number <code>19</code>, so, for example, <code>ping
+			021.0.0.1</code> would ping <code>17.0.0.1</code>. <code>ip_addr</code>
+		also supports hexadecimal values, allowing the user to enter an ip
+		address such as <code>0xa.012.34.0xF</code> which resolves to the IP
+		address: <code>10.10.34.15</code>. Arlith may, in the future, support
+		these notations for IPv4 octets.
+	</span>
 </p>
+<h3>IPv6</h3>
 <p>
-	Domain names should be any valid domain name. Currently, Arlith parses
-	this by simply checking for at least two parts, each separated by a dot
-	(<code>.</code>) (technically referred to as <i>labels</i>). Each part
-	should be composed of alphanumeric characters and hyphens, but such
-	that no part should begin or end with a hyphen. Furthermore, no part
-	should exceed 63 characters in length and a whole domain name should
-	not exceed 253 characters.
+	Arlith does not currently support IPv6 notation. If brackets are used
+	for the domain and the beginning of the string contained by those
+	brackets is <code>ipv6::</code> in any capitalization, Arlith will
+	reject the email address, citing IPv6 usage.
 </p>
+
+<h3>General Constraints</h3>
 <p>
-	IPv4 addresses should be a valid IPv4 string. The <code>ip-v6</code>
-	non-terminal in the rules above expands to any valid IPv6 address with
-	the prefix <code>IPv6</code> in front. The <code>ip-v6</code> address
-	non-terminal is currently not supported by Arlith.
+	Each label in a domain name is limited to <code>63</code> characters in
+	length. Additionally, the entire domain part, assuming a domain name is
+	used, is limited to <code>253</code> characters in length.
 </p>
+
+<h2>Production Rule Syntax</h2>
 <p>Each part of the rules above is color coded:</p>
 <ul>
 	<li class="production">Non-Terminals</li>
 	<li class="chars">Character Literal Values and Character Literal Ranges</li>
 	<li class="described-production">Described Non-Terminals</li>
-	<li class="repl">"Replicators" (e.g. <i>Kleene Star</i>, *)
-	</li>
-	<li>Syntactical Content</li>
-	<li><span class="optional">Optional Content (Hover Over Me)</span></li>
 </ul>
 <h3>Character Literals &amp; Ranges</h3>
 <p>
