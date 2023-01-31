1edfb4928a4032b5c0b5bc9912e7b65e15c28db7
Palanath <pala@dusttoash.org>
2023-01-30 23:38:55 -0600

    Update Presentation Framework wiki page

--- arlith.net/presentation-framework/index.php
+++ arlith.net/presentation-framework/index.php
@@ -4,7 +4,7 @@
 	The Presentation Framework is a framework for making user interfaces
 	that's designed to maximize the ability to <i>skin</i> (or theme or
 	style) user interfaces, with little redundancy in user interface logic.
-	The framework is chiefly used by frontends using the <a
+	The framework is chiefly used by frontends that use the <a
 		href="/frontend-api">Frontend API</a>. The framework considers a user
 	interface as two two conceptual parts:
 </p>
@@ -18,11 +18,28 @@
 		information or commands to the Logic are to be shown to the user.</li>
 </ul>
 <p>
-	Typically, a user interface will have one Logic implementation.
-	Presentation implementations for a user interface are provided by <em>Themes</em>
-	that choose to support the user interface; when a user interface is
-	shown, an instance of its logic is instantiated and a Theme is queried
-	to provide a corresponding presentation implementation. Once a
-	presentation implementation is obtained, it is linked with the logic
-	implementation (if necessary), and then shown to the user.
+	Typically, a user interface will have one Logic implementation and
+	multiple Presentation implementations for a user interface will be
+	provided by <em>Themes</em> that choose to support the user interface.
+	When a Frontend determines that a user interface needs to be shown, an
+	instance of its Logic is instantiated and a Theme is queried to provide
+	a corresponding presentation implementation. Once a presentation
+	implementation is obtained, it is linked with the logic implementation
+	(if necessary), and then shown to the user. The Client Frontend keeps
+	an ordered list of Themes, sorted by priority, for generating
+	presentations for interfaces. When an interface needs to be shown, it
+	queries each Theme, in order of priority, until it finds a Theme that
+	can successfully generate a Presentation instance for the interface.
+</p>
+<h2>Creating User Interfaces</h2>
+<p>
+	Each user interface should have a corresponding Logic <code>interface</code>
+	and Presentation <code>interface</code>.
+</p>
+
+<h2>Implementation Notes</h2>
+<p>
+	The Presentation Framework is <i>currently contained within the Client
+		Frontend</i>. It should, at some point, be moved to a more generic, <code>libraries</code>
+	sub-package.
 </p>
