ae8ff6bb8c5a8315b0faa887ec2c050ba33628f2
Palanath <pala@dusttoash.org>
2023-01-31 01:39:40 -0600

    Update Presentation Framework wiki page

--- arlith.net/presentation-framework/index.php
+++ arlith.net/presentation-framework/index.php
@@ -3,8 +3,8 @@
 <p>
 	The Presentation Framework is a framework for making user interfaces
 	that's designed to maximize the ability to <i>skin</i> (or theme or
-	style) user interfaces, with little redundancy in user interface logic.
-	The framework is chiefly used by frontends that use the <a
+	style) user interfaces, with little redundancy in user interface (UI)
+	logic. The framework is chiefly used by frontends that use the <a
 		href="/frontend-api">Frontend API</a>. The framework considers a user
 	interface as two two conceptual parts:
 </p>
@@ -31,12 +31,60 @@
 	queries each Theme, in order of priority, until it finds a Theme that
 	can successfully generate a Presentation instance for the interface.
 </p>
-<h2>Creating User Interfaces</h2>
+<h2>User Interface Structure</h2>
 <p>
 	Each user interface should have a corresponding Logic <code>interface</code>
-	and Presentation <code>interface</code>.
+	and Presentation <code>interface</code>. Conventionally, these
+	interfaces are named based on the name of the user interface, for
+	example, for the Home scene in Arlith (that shows immediately after
+	logging in), the interfaces are <code>HomeLogic</code> and <code>HomePresentation</code>.
+	These interfaces extend <code>Logic&lt;HomePresentation&gt;</code> and
+	<code>Presentation&lt;HomeLogic&gt;</code>, respectively. Typically,
+	the two interfaces are designed so that the Presentation has methods
+	that allow the Logic to push information to the UI (to show to the
+	user; commonly called <i>show</i> methods), and query information from
+	the UI (to accept user input for, e.g., processing; commonly called <i>get</i>
+	methods), and so that the Logic has methods that allow the Presentation
+	to <i>trigger</i> certain actions in the Logic, (these reflect a user
+	invoking some action in the logic, e.g. when the user pushes a button
+	to submit some data).
 </p>
-
+<h3>Creating a UI</h3>
+<p>Broadly, these are the steps to creating a UI under the framework:</p>
+<ol>
+	<li>Create interface subtypes <code>NameLogic extends
+			Logic&lt;NamePresentation&gt;</code> and <code>NamePresentation
+			extends &lt;NameLogic&gt;</code>, where <code>Name</code> is the name
+		of the UI.
+		<ul>
+			<li>Populate the logic with any <b>trigger</b> methods needed to let
+				the user invoke actions through the UI.
+			</li>
+			<li>Populate the presentation with any <b>get</b> methods and <b>show</b>
+				methods needed to allow the UI to grab and show any information it
+				needs.
+			</li>
+		</ul>
+	</li>
+	<li>Create an implementation class of <code>NameLogic</code>, typically
+		<code>NameLogicImpl implements NameLogic</code>.<sup info=1></sup><span
+		info=1>When this UI needs to be shown, say, by a previous UI, an
+			instance of the logic implementation is provided to a Theme to select
+			an appropriate presentation.</info>
+	</span>
+	</li>
+	<li>Implement <code>NamePresentation</code> under a theme, typically as
+		<code>NamePresentationImpl implements NamePresentation</code>.
+		<ul>
+			<li>The <code>NamePresentationImpl</code> class should implement the
+				<code>getScene()</code> method, (declared by <code>Presentation</code>),
+				which generates the JavaFX <code>Scene</code> to be shown to the
+				user.<sup info=2></sup><span info=2>The Frontend uses this method to
+					grab the scene and display it in the currently active window so
+					that the presentation is shown to the user.</span></li>
+		</ul>
+	</li>
+</ol>
 <h2>Implementation Notes</h2>
 <p>
 	The Presentation Framework is <i>currently contained within the Client
